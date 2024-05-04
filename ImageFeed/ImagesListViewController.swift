16dcb125eb055f5c0d96acb636ce6b29edb58bd9
EntityVadim <21930@mail.ru>
2024-05-04 15:22:09 +0300

    Создал TableView и настроил его, переименовал файл ViewController.swift в ImagesListViewController.swift, а класс, который в нём находится — в ImagesListViewController, поменял название класса на ImagesListViewController в Main.Storyboard, поменял название сцены в сториборде на ImageList, также для View этой сцены изменил Background Color на тот, что в макетах

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
