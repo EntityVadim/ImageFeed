945a317e722b24247b625dc012572b4546b8fe82
EntityVadim <21930@mail.ru>
2024-05-04 15:29:48 +0300

    Добавил два свойства UITableViewDelegate и UITableViewDataSource в ImagesListViewController

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
-
+    
     @IBOutlet private var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -9,3 +9,17 @@ final class ImagesListViewController: UIViewController {
     }
 }
 
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
