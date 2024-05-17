268b6d91239f6d1ad7295514bbafbc8351ef4f3d
EntityVadim <21930@mail.ru>
2024-05-17 09:13:59 +0300

    Создал новый Swift-файл с названием SingleImageViewController, добавил новый контроллер в Main.storyboard, сделал переход Push на экран с картинкой.

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Вадим on 17.05.2024.
+//
+
+import UIKit
+
+// MARK: - SingleImageViewController
+
+final class SingleImageViewController: UIViewController{
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
