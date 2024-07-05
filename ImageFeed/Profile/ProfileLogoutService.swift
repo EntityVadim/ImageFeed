83c2da55811deba4fd8ddaeb52a9c44ae2317a6c
EntityVadim <21930@mail.ru>
2024-07-05 17:20:16 +0300

    - Выполнил пункт (по нажатию на кнопку выхода нужно показать предупреждение и запросить подтверждение выхода; если пользователь подтверждает выход из аккаунта)

--- ImageFeed/Profile/ProfileLogoutService.swift
+++ ImageFeed/Profile/ProfileLogoutService.swift
@@ -14,9 +14,17 @@ final class ProfileLogoutService {
     private init() { }
     
     func logout() {
-        cleanCookies()
-        clearLocalData()
-        switchToSplashViewController()
+        let alert = UIAlertController(title: "Выход из аккаунта", message: "Вы уверены, что хотите выйти?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Подтвердить", style: .destructive, handler: { _ in
+            self.cleanCookies()
+            self.clearLocalData()
+            self.switchToSplashViewController()
+        }))
+        alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            rootViewController.present(alert, animated: true, completion: nil)
+        }
     }
     
     private func cleanCookies() {
